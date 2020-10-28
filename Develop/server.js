a801d2a2122f2a15c30f74ff0ac00fd3b70477fc
Sankung Sillah <ssillah306@gmail.com>
2020-10-27 21:27:15 -0400

    requiring the apiRoutes

--- Develop/server.js
+++ Develop/server.js
@@ -6,3 +6,6 @@ const PORT = process.env.PORT || 8080;
 // setting up the express app to handle data parsing
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+// requiring the routes
+require("./routes/apiRoutes")(app);
