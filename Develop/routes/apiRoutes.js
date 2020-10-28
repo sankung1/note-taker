c646141b4b21196a997b9db523912d794c74ecf2
Sankung Sillah <ssillah306@gmail.com>
2020-10-27 20:33:24 -0400

    get added note and update the db file

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -7,4 +7,11 @@ module.exports = app =>{
     app.get("/api/notes", (req,res) =>{
         res.json(notes);
     })
+    
+    // setting up a post route for notes api route
+    app.post("/api/notes", (req, res) =>{
+        let addNote = req.body;
+        notes.push(addNote);       
+    })
+
 }
