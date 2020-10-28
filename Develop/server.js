8933062977de13c771f92de0d7aa14760e0c34e7
Sankung Sillah <ssillah306@gmail.com>
2020-10-27 21:24:41 -0400

    setting up the express app to handle data parsing

--- Develop/server.js
+++ Develop/server.js
@@ -2,3 +2,7 @@ const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// setting up the express app to handle data parsing
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
