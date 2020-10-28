caf806caa0259b7d8020325834a6549d6e3468b4
Sankung Sillah <ssillah306@gmail.com>
2020-10-27 20:40:56 -0400

    added a readFile to read the db.json data

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,21 +1,24 @@
 const fs = require("fs");
 const path = require("path");
 
-module.exports = app =>{
+module.exports = app => {
+    fs.readFile("db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
 
-    // setting up api route for notes
-    app.get("/api/notes", (req,res) =>{
-        res.json(notes);
-    })
-    
-    // setting up a post route for notes api route
-    app.post("/api/notes", (req, res) =>{
-        let addNote = req.body;
-        notes.push(addNote);       
-    })
-    // getting the unique id of the added note
-    app.get("/api/notes/:id", (req,res)=>{
-        res.json(notes[req.params.id]);
-    })
+        // setting up api route for notes
+        app.get("/api/notes", (req, res) => {
+            res.json(notes);
+        })
 
+        // setting up a post route for notes api route
+        app.post("/api/notes", (req, res) => {
+            let addNote = req.body;
+            notes.push(addNote);
+        })
+        // getting the unique id of the added note
+        app.get("/api/notes/:id", (req, res) => {
+            res.json(notes[req.params.id]);
+        })
+    })
 }
