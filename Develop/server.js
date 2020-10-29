aa7a39121b330cf0859a123f4434497b14810cf0
Sankung Sillah <ssillah306@gmail.com>
2020-10-28 20:38:21 -0400

    updated the port

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // setting up the express app to handle data parsing
 app.use(express.urlencoded({extended: true}));
